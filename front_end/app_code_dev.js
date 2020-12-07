d3688868766f28ed1a225d3eea40729de2c9bef5
TayseerRahman <55595201+TayseerRahman@users.noreply.github.com>
2020-12-06 22:33:47 -0500

    Update app_code_dev.js

--- front_end/app_code_dev.js
+++ front_end/app_code_dev.js
@@ -31,6 +31,8 @@ var updateView = async (button) => {
         api = `https://${URL}/api/combined_query/${queryName}/${queryLevel}`;
     }
    
+    const proxyurl = "https://cors-anywhere.herokuapp.com/"
+    const data = await fetch(proxyurl+api);
     const data = await fetch(api);
     const model = await data.json();
     render_view(model);
