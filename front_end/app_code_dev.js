53e3e57adb2917057128555f8b32a8bcdd37c5e5
TayseerRahman <55595201+TayseerRahman@users.noreply.github.com>
2020-12-06 19:42:45 -0500

    Update app_code_dev.js

--- front_end/app_code_dev.js
+++ front_end/app_code_dev.js
@@ -2,32 +2,34 @@
 //CUS1172 - Final Project
 //app_code_dev.js
 
+var URL = window.location.hostname;
 var updateView = async (button) => {
     //Code Query
     if (button.dataset.querytype == 'by_course_code') {
         let queryvalue = document.querySelector('#codeQuery').value;
-        api = `http://localhost:3000/api/by_course_code/${queryvalue}`;
+        api = `https://${URL}/api/by_course_code/${queryvalue}`;
     }
     //Title Query
     if (button.dataset.querytype == 'by_title') {
         let queryvalue = document.querySelector('#titleQuery').value;
-        api = `http://localhost:3000/api/by_title/${queryvalue}`;
+        api = `https://${URL}/api/by_title/${queryvalue}`;
     }
     //Instructor Query
     if (button.dataset.querytype == 'by_instructor') {
         let queryvalue = document.querySelector('#instructorQuery').value;
-        api = `http://localhost:3000/api/by_instructor/${queryvalue}`;
+        api = `https://${URL}/api/by_instructor/${queryvalue}`;
     }
     //Level Query
     if (button.dataset.querytype == 'by_level') {
         let queryvalue = document.querySelector('#levelQuery').value;
-        api = `http://localhost:3000/api/by_level/${queryvalue}`;
+        api = `https://${URL}/api/by_level/${queryvalue}`;
     }
     //Combined Query
     if (button.dataset.querytype == 'combined_query') {
         let queryName = document.querySelector('#nameQuery').value;
         let queryLevel = document.querySelector('#levQuery').value;
         api = `http://localhost:3000/api/combined_query/${queryName}/${queryLevel}`;
+        api = `https://${URL}/api/combined_query/${queryName}/${queryLevel}`;
     }
 
     const data = await fetch(api);
